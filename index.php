8eec544fcd15ed22c5e4669b0fa0e1e1b26fd194
Deep Rakesh <deeptank09@gmail.com>
2022-09-23 12:03:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,13 +26,13 @@
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
                         <li class="nav__item">
-                          <a href="/responsive-gym-website-main/index.php" class="nav__link active-link">Home</a>
+                          <a href="/index.php" class="nav__link active-link">Home</a>
                         </li>
                         <li class="nav__item">
-                            <a href="/responsive-gym-website-main/aboutus.php" class="nav__link">About Us</a>
+                            <a href="/aboutus.php" class="nav__link">About Us</a>
                           </li>
                           <li class="nav__item">
-                            <a href="/responsive-gym-website-main/contactus.php" class="nav__link">Contact Us</a>
+                            <a href="/contactus.php" class="nav__link">Contact Us</a>
                           </li>
                           
 
